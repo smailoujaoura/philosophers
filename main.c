713dcf005ec9ab3a49a0353de97d5611d4b7514d
Smail Oujaoura <email.oujaoura@gmail.com>
2025-03-08 08:49:04 +0000

    philo taking no shape

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: soujaour <soujaour@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/08 14:37:01 by soujaour          #+#    #+#             */
-/*   Updated: 2025/03/07 21:48:23 by soujaour         ###   ########.fr       */
+/*   Updated: 2025/03/08 08:16:43 by soujaour         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,7 +65,7 @@ void	*start_routine(void *ptr)
 	t_philo	*philo;
 
 	philo = (t_philo*)ptr;
-	while (!philo->sync->stop)
+	while (1)
 	{
 		eating(philo);
 
@@ -137,9 +137,9 @@ int	start_sync(t_philo *philos, t_sync *sync)
 		while (++i < sync->total_philos)
 		{
 			pthread_mutex_lock(&sync->lock);
-			if (get_time() - philos[i].last_meal >= sync->death_time)
+			if (get_time() - philos[i].last_meal > sync->death_time)
 			{
-				sync->stop = 1;
+				//sync->stop = 1;
 				pthread_mutex_lock(&sync->write);
 				printf("%zu ms %zu has died\n", get_time() - sync->starting_time, philos[i].philo_number);
 				return (0);
@@ -148,9 +148,10 @@ int	start_sync(t_philo *philos, t_sync *sync)
 		}
 		if (sync->cycles_total > 0 && has_reached_total_cycles(philos, sync))
 		{
-			sync->stop = 1;
-			break ;
+			//sync->stop = 1;
+			return (0);
 		}
+		usleep(1000);
 	}
 	pthread_mutex_unlock(&sync->write);
 	return (0);
